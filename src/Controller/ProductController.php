70ead5eee704cef16f23fcea9aec0d7948efde5c
Mathilde Morell <morell.mathilde74@gmail.com>
2024-12-27 18:43:49 +0100

    show product fix problem

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -4,46 +4,62 @@ namespace App\Controller;
 
 use App\Entity\Product;
 use App\Form\ProductType;
+use App\Repository\ProductRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\File\Exception\FileException;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Attribute\Route;
-use Symfony\Component\HttpFoundation\File\UploadedFile;
+use Symfony\Component\Routing\Annotation\Route;
 
 class ProductController extends AbstractController
 {
-    #[Route('/product', name: 'app_product')]
-    public function index(EntityManagerInterface $em, Request $request): Response
+    #[Route('/product/{id}', name: 'product_show', methods: ['GET'])]
+    public function show(int $id, ProductRepository $productRepository): Response
+    {
+        $product = $productRepository->find($id);
+
+        if (!$product) {
+            throw $this->createNotFoundException('Le produit n\'existe pas.');
+        }
+
+        return $this->render('product/product.html.twig', [
+            'product' => $product,
+        ]);
+    }
+
+    #[Route('/product', name: 'app_product', methods: ['GET', 'POST'])]
+    public function createOrList(EntityManagerInterface $em, Request $request): Response
     {
         $product = new Product();
         $form = $this->createForm(ProductType::class, $product);
         $form->handleRequest($request);
-        if($form->isSubmitted() && $form->isValid()){
 
+        if ($form->isSubmitted() && $form->isValid()) {
             /** @var UploadedFile $imageFile */
             $imageFile = $form->get('image')->getData();
- 
+
             if ($imageFile) {
-                $newFilename = uniqid().'.'.$imageFile->guessExtension();
- 
+                $newFilename = uniqid() . '.' . $imageFile->guessExtension();
+
                 try {
                     $imageFile->move(
                         $this->getParameter('upload_directory'),
                         $newFilename
                     );
                 } catch (FileException $e) {
-                    $this->addFlash('error', "Impossible d'ajouter l'image");
+                    $this->addFlash('error', 'Impossible de téléverser l\'image.');
                     return $this->redirectToRoute('app_product');
                 }
- 
+
                 $product->setImage($newFilename);
             }
 
             $em->persist($product);
             $em->flush();
-            $this->addFlash('success', 'Produit ajouté');
+
+            $this->addFlash('success', 'Produit ajouté avec succès.');
             return $this->redirectToRoute('app_product');
         }
 
@@ -51,24 +67,27 @@ class ProductController extends AbstractController
 
         return $this->render('product/index.html.twig', [
             'products' => $products,
-            'ajout_produit' => $form
+            'product_form' => $form->createView(),
         ]);
     }
 
-    #[Route('/product/delete/{id}', name: 'app_product_delete')]
-    public function delete(Request $request, EntityManagerInterface $em, Product $product = null)
+    #[Route('/product/delete/{id}', name: 'app_product_delete', methods: ['POST'])]
+    public function delete(Request $request, EntityManagerInterface $em, Product $product = null): Response
     {
-        if($product == null){
-            $this->addFlash('error', 'Produit introuvable');
+        if (!$product) {
+            $this->addFlash('error', 'Produit introuvable.');
             return $this->redirectToRoute('app_product');
         }
 
-        if ($this->isCsrfTokenValid('delete' . $product->getId(), $request->request->get('csrf'))) {
+        if ($this->isCsrfTokenValid('delete' . $product->getId(), $request->request->get('_token'))) {
             $em->remove($product);
             $em->flush();
-            
-            $this->addFlash('success', 'Produit supprimé');
+
+            $this->addFlash('success', 'Produit supprimé avec succès.');
+        } else {
+            $this->addFlash('error', 'Token CSRF invalide.');
         }
+
         return $this->redirectToRoute('app_product');
     }
 }
