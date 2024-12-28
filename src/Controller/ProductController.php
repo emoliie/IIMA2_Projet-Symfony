f6656cd4eb65459e1b2c9573537a4540aef495d3
Emilie Xu <emilie.xu@outlook.com>
2024-12-28 15:59:52 +0100

    :sparkles: Add edit product

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -40,4 +40,43 @@ class ProductController extends AbstractController
             'product' => $product,
         ]);
     }
+
+    // Modifie le produit
+    #[Route('/product/edit/{id}', name: 'product_edit', methods: ['GET', 'POST'])]
+    public function edit(
+        Product $product,
+        Request $request,
+        EntityManagerInterface $em
+    ): Response {
+        // Vérifie que l'utilisateur a les droits nécessaires
+        $this->denyAccessUnlessGranted('ROLE_ADMIN');
+
+        // Crée le formulaire pour modifier le produit
+        $form = $this->createForm(ProductFormType::class, $product);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            // Gère l'upload d'une nouvelle image si elle est modifiée
+            $imageFile = $form->get('image')->getData();
+            if ($imageFile) {
+                $newFilename = uniqid() . '.' . $imageFile->guessExtension();
+                $imageFile->move(
+                    $this->getParameter('upload_directory'),
+                    $newFilename
+                );
+                $product->setImage($newFilename);
+            }
+
+            // Sauvegarde les modifications
+            $em->flush();
+
+            $this->addFlash('success', 'Produit modifié avec succès.');
+            return $this->redirectToRoute('product_show', ['id' => $product->getId()]);
+        }
+
+        return $this->render('product/edit.html.twig', [
+            'form' => $form->createView(),
+            'product' => $product,
+        ]);
+    }
 }
